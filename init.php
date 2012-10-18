d955364bddd4c955041b2103a3132a6e23d24b1d
ha1t <halt.feits@gmail.com>
2012-10-18 20:19:30 +0900

    fatalがでないレベルまでコメントアウト

--- init.php
+++ init.php
@@ -1,12 +1,4 @@
 <?php
-define('GOOGLE_CLIENT_ID', 'your_google_client_id');
-define('GOOGLE_CLIENT_SECRET', 'your_google_client_secret');
-define('GOOGLE_REDIRECT_URI', APP_URL . 'callback_path');
-define('GOOGLE_DEVELOPER_KEY', 'your_google_account');
-
-define('GOOGLE_ALLOW_DOMAINS', 'example.com, example.jp');
-define('GOOGLE_ALLOW_EMAILS', 'example-com@gmail.com, example-jp@gmail.com');
-
 $menuContainer = ClassRegistry::getObject('MenuContainer');
 $menuContainer->addTopMenu(
 	array(
@@ -36,28 +28,28 @@ $hookContainer->registerElementHook(
 
 $settingContainer = ClassRegistry::getObject('SettingContainer');
 $settingContainer->addSystemSetting(array(
-	'name' => 'cc_facebook',
-	'partial' => '../../Plugin/CcFacebook/View/Element/settings',
+	'name' => 'cc_google_auth',
+	'partial' => '../../Plugin/CcGoogleAuth/View/Element/settings',
 	'label' => __('Facebook Plugin')
 ));
 
-if (!class_exists('Facebook')) {
-	require_once 'Vendor/facebook-php-sdk/src/facebook.php';
-}
+//if (!class_exists('Facebook')) {
+//	require_once 'Vendor/facebook-php-sdk/src/facebook.php';
+//}
 
 $Setting = ClassRegistry::init('Setting');
-$facebook = new Facebook(
-	array(
-		'appId' => $Setting->fb_appid,
-		'secret' => $Setting->fb_secret,
-	)
-);
-ClassRegistry::addObject('Facebook', $facebook);
 
+//$facebook = new Facebook(
+//	array(
+//		'appId' => $Setting->fb_appid,
+//		'secret' => $Setting->fb_secret,
+//	)
+//);
+//ClassRegistry::addObject('Facebook', $facebook);
 
 App::uses('CakeEventManager', 'Event');
-App::uses('ServiceFB', 'CcFacebook.Vendor');
-$service = new ServiceFB();
-CakeEventManager::instance()->attach(array($service,'afterIssueNewhandler'), 'Controller.Candy.issuesNewAfterSave');
-CakeEventManager::instance()->attach(array($service,'afterIssueEdithandler'), 'Controller.Candy.issuesEditAfterSave');
+//App::uses('ServiceFB', 'CcFacebook.Vendor');
+//$service = new ServiceFB();
+//CakeEventManager::instance()->attach(array($service,'afterIssueNewhandler'), 'Controller.Candy.issuesNewAfterSave');
+//CakeEventManager::instance()->attach(array($service,'afterIssueEdithandler'), 'Controller.Candy.issuesEditAfterSave');
 
